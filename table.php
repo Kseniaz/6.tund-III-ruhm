3d354c4cc53e6194a540e7f05f2523eab8eefa10
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-07 12:35:49 +0300

    Added table

--- table.php
+++ table.php
@@ -1,9 +1,31 @@
 <?php
 	require_once("functions.php");
 	
-	//k�ivitan funktsiooni
+	//käivitan funktsiooni
 	$array_of_cars = getCarData();
 	
-	//tr�kin v�lja esimese auto
-	echo $array_of_cars[0]->id." ".$array_of_cars[0]->plate;
+	//trükin välja esimese auto
+	//echo $array_of_cars[0]->id." ".$array_of_cars[0]->plate;
 ?>
+
+<h2>Tabel</h2>
+<table border=1 >
+	<tr>
+		<th>id</th>
+		<th>numbrimärk</th>
+	</tr>
+	
+	<?php
+		// trükime välja read
+		// massiivi pikkus count()
+		for($i = 0; $i < count($array_of_cars); $i++){
+			//echo $array_of_cars[$i]->id;
+			echo "<tr>";
+			echo "<td>".$array_of_cars[$i]->id."</td>";
+			echo "<td>".$array_of_cars[$i]->plate."</td>";
+			echo "</tr>";
+		}
+	
+	?>
+</table>
+
