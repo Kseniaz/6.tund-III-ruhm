c8cb3416f1e7a29aa14e0e1702392a5b5cf397b8
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-07 13:40:25 +0300

    editing table with update fn

--- table.php
+++ table.php
@@ -12,6 +12,12 @@
 		
 	}
 	
+	//salvestan andmebaasi uuendused
+	if(isset($_POST["save"])){
+		
+		updateCar($_POST["id"], $_POST["plate_number"], $_POST["color"]);
+	}
+	
 	
 	//käivitan funktsiooni
 	$array_of_cars = getCarData();
@@ -43,12 +49,15 @@
 			if(isset($_GET["edit"]) && $array_of_cars[$i]->id == $_GET["edit"]){
 				
 				echo "<tr>";
+				echo "<form action='table.php' method='post'>";
+				echo "<input type='hidden' name='id' value='".$array_of_cars[$i]->id."'>";
 				echo "<td>".$array_of_cars[$i]->id."</td>";
 				echo "<td>".$array_of_cars[$i]->user_id."</td>";
 				echo "<td><input name='plate_number' value='".$array_of_cars[$i]->plate."'></td>";
 				echo "<td><input name='color' value='".$array_of_cars[$i]->color."'></td>";
-				echo "<td><a href='table.php'>cancel<a></td>";
+				echo "<td><a href='table.php'>cancel</a></td>";
 				echo "<td><input type='submit' name='save'></td>";
+				echo "</form>";
 				echo "</tr>";
 				
 			}else{
